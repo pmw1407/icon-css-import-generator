5ead668262413c1c9b073856a644799f23caf1b8
Jungmin <pmw1407@hanyang.ac.kr>
2023-06-20 14:16:42 +0900

    icon import script

---
+++ src/icon-css-import.ts
@@ -0,0 +1,33 @@
+import finder from 'find-package-json'
+import { readdirSync } from 'fs'
+
+const packageInfo = finder().next().value
+
+const config = packageInfo['icon-import-generator']
+
+const destination = config['destination']
+const root = config['root']
+
+const newLine = '\n'
+
+const makeAvailableIconNames = (iconName: string) => {
+  return `${iconName},${newLine}`
+}
+
+let content = 'export const availableDesignedIconNames = [\n'
+
+const generateAvailableIconNames = () => {
+  const iconFiles = readdirSync(root, { withFileTypes: true })
+
+  iconFiles.forEach((_file) => {
+    const fileName = _file.name.split('.')[0]
+
+    content += makeAvailableIconNames(fileName)
+  })
+
+  content += '] as const'
+}
+
+generateAvailableIconNames()
+
+console.log(content)
